fde80a6807c57008904b9157be0be7a7d4309bfa
Vince-PC\Vince <pandavigoureux29@gmail.com>
2017-10-30 17:43:33 -0400

    - reading elements and attack types

--- Assets/scripts/data/database/DataGameManager.cs
+++ Assets/scripts/data/database/DataGameManager.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class DataGameManager : DatabaseLoader
 {
     AccuracyScoringData m_accuracyScoring;
+    ElementsDataCollection m_elements;
+    AttackTypeDataCollection m_attackTypes;
 
     protected override void LoadDatabase()
     {
@@ -12,6 +14,8 @@ public class DataGameManager : DatabaseLoader
         var tempDatabase = LoadDataJSON("game_database");
         var accJSON = tempDatabase["accuracy_scoring"];
         m_accuracyScoring = new AccuracyScoringData(accJSON[0]);
+        m_elements = JSONLoaderLR.LoadTable<ElementsDataCollection>(tempDatabase["elements"]);
+        m_attackTypes = JSONLoaderLR.LoadTable<AttackTypeDataCollection>(tempDatabase["attack_type"]);
     }
 
     public class AccuracyScoringData : JSONData
@@ -31,6 +35,50 @@ public class DataGameManager : DatabaseLoader
             }
         }
     }
+
+    #region ELEMENTS
+    public class ElementData : JSONData
+    {
+        public string Name;
+        public string Weakness;
+        public string Prefab;
+
+        public ElementData() { }
+        public ElementData(JSONObject _json) : base(_json) { }
+
+        public override void BuildJSONData(JSONObject _json)
+        {
+            base.BuildJSONData(_json);
+            Name = _json.GetField("name").str;
+            Prefab = _json.GetField("prefab").str;
+            Weakness = _json.GetField("weakness").str;
+        }
+    }
+
+    public class ElementsDataCollection : IJSONDataDicoCollection<ElementData> { }
+
+    #endregion
+
+    #region ATTACK_TYPE
+    public class AttackTypeData : JSONData
+    {
+        public string Name;
+        public string Prefab;
+
+        public AttackTypeData() { }
+        public AttackTypeData(JSONObject _json) : base(_json) { }
+
+        public override void BuildJSONData(JSONObject _json)
+        {
+            base.BuildJSONData(_json);
+            Name = _json.GetField("name").str;
+            Prefab = _json.GetField("prefab").str;
+        }
+    }
+
+    public class AttackTypeDataCollection : IJSONDataDicoCollection<AttackTypeData> { }
+
+    #endregion
 
     public AccuracyScoringData AccuracyScoring { get { return m_accuracyScoring; } }
 }
