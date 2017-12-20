56d74009521f31a734a381cd5b6fb7ea618621f5
GAMELOFT\vincent.roumier <pandavigoureux29@gmail.com>
2017-12-20 08:53:40 -0500

    - first draft of begin battle screen

---
+++ Assets/scripts/UI/UITextAndImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextAndImage : MonoBehaviour {
+
+    [SerializeField] protected Text m_text;
+    [SerializeField] protected Image m_image;
+
+    public void SetText(string _text)
+    {
+        m_text.text = _text;
+    }
+
+    public void SetImage(Sprite _sprite)
+    {
+        m_image.sprite = _sprite;
+    }
+}
---
+++ Assets/scripts/menus/menus/CharacterInfosUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterInfosUI : MonoBehaviour {
+
+    [SerializeField] protected int m_characterIndexInProfile;
+    [SerializeField] protected float m_uiCharaScale;
+
+    [SerializeField] protected UITextAndImage m_metaValue;
+    [SerializeField] protected GameObject m_elementAttribute;
+    [SerializeField] protected GameObject m_attackAttribute;
+    [SerializeField] protected GameObject m_attribute;
+
+    public GameObject CharacterObject;
+    public Text LevelText;
+
+    // Use this for initialization
+    void Start () {
+        LoadData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadData()
+    {
+        var m_charManager = DataManager.instance.CharacterManager;
+
+        //get infos for the character
+        var teamMates = ProfileManager.instance.GetCurrentTeam();
+        var mate = teamMates[m_characterIndexInProfile];
+        var levelUpData = m_charManager.GetNextLevelByXp(mate.Job, mate.Xp);
+
+        var go = GameUtils.CreateCharacterUIObject(mate, m_uiCharaScale);
+        go.transform.SetParent( CharacterObject.transform, false);
+
+        LevelText.text = "" + levelUpData.Stats.Level; 
+    }
+}
